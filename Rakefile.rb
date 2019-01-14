4bb73049f52980eb2f86824c46b9c67f22f74f64
James Turnbull <james@lovedthanlost.net>
2019-01-14 02:50:44 -0500

    Updated versions and added license

--- Rakefile.rb
+++ Rakefile.rb
@@ -14,12 +14,13 @@ gemspec = Gem::Specification.new do |s|
   s.name = 'riemann-hadoop'
   s.version = '0.1.1'
   s.author = 'Pradeep Chhetri'
+  s.license = 'MIT'
   s.email = 'pradeep.chhetri89@gmail.com'
   s.homepage = 'https://github.com/riemann/riemann-hadoop'
   s.platform = Gem::Platform::RUBY
   s.summary = 'Collect jmx stats for Hadoop Namenode and Datanode and submits them to Riemann.'
 
-  s.add_dependency 'riemann-tools', '>= 0.2.1'
+  s.add_dependency 'riemann-tools', '~> 0.2.18'
 
   s.files = FileList['bin/*', 'LICENSE', 'README.md'].to_a
   s.executables |= Dir.entries('bin/')
